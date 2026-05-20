b4987c7f1d6d00f19178d670180bccca922d33d1
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-05-20 17:27:35 -0300

    feat
     adiciona rodapé completo com redes sociais e ajustes de responsividade

---
+++ script.js
@@ -0,0 +1,14 @@
+function enviarWatsApp(event) {
+  event.preventDefault(); // Impede o envio do formulário padrão
+
+const nome = document.getElementById("nome");
+const mensagem = document.getElementById("mensagem");
+const telefone = "5554991015900";
+
+const texto = `Olá, meu nome é ${nome.value}, ${mensagem.value}`;
+const msgFormatada = encodeURIComponent(texto);
+const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${msgFormatada}`;
+window.open(url, "_blank");
+nome.value = "";
+mensagem.value = "";
+}
